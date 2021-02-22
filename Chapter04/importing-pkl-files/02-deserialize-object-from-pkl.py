b8fb0a5e6d96e62f30d83db17949f4a8ea1165f1
Luca Zavarella <lucazavarella@outlook.com>
2021-02-22 22:00:01 +0100

    added other comments

--- Chapter04/importing-pkl-files/02-deserialize-object-from-pkl.py
+++ Chapter04/importing-pkl-files/02-deserialize-object-from-pkl.py
@@ -1,8 +1,11 @@
 import pandas as pd
 import pickle
 
+# Deserialize your dictionary using the load function of pickle
 deserialized_dict = pickle.load( open("nested_population_dict.pkl", "rb") )
 
+# Get the dataframe of data related to Sweden
 sweden_population_df = deserialized_dict['Sweden']
 
+# Show the data
 print(sweden_population_df)
--- Chapter04/importing-pkl-files/04-create-plots-object-to-serialize-in-pkl.py
+++ Chapter04/importing-pkl-files/04-create-plots-object-to-serialize-in-pkl.py
@@ -3,32 +3,52 @@ import pandas as pd
 import pickle
 import matplotlib.pyplot as plt
 
+# Load the data into a dataframe
 population_df = pd.read_csv("population.csv")
 
-
+# Define a list of selected countries
 selected_countries = ["Italy", "Sweden", "France", "Germany"]
 
+# Let's create a dataframe having only rows for selected countries.
+# The isin() method helps in selecting rows with having a particular (or multiple) value(s) in a particular column
 selected_population_df = population_df.loc[ population_df['country'].isin(selected_countries) ]
 
+# Define an empty dictionary
 nested_population_dict = {}
+
+# For each country...
 for c in selected_countries:
+
+    # ... organize its 'year' and 'population' data in a dataframe and
+    # insert it in the dictionary in correspondence of the country name
     nested_population_dict[c] = selected_population_df.loc[ population_df['country'] == c ].filter(['year', 'population']).reset_index(drop=True)
 
+# Let's try to plot the time series for Sweden
 selected_country = "Sweden"
 x = nested_population_dict[selected_country].year
 y = nested_population_dict[selected_country].population
 
+# Create a figure object
 fig_handle = plt.figure()
+# Plot a simple line for each (x,y) point
 plt.plot(x, y)
+# Add a title to the figure
 plt.title("Global population of " + selected_country)
 
+# Show the figure
 fig_handle.show()
 
 
+# Define another empty dictionary that will contain the pictures for each country
 nested_population_plots_dict = {}
+
+# For each country...
 for c in selected_countries:
-    tmp_df = selected_population_df.loc[ population_df['country'] == c ].filter(['year', 'population']).reset_index(drop=True)
     
+    # ...get its time series dataframe using the previous dictionary
+    tmp_df = nested_population_dict[c]
+    
+    # Prepare the plot
     x = tmp_df.year
     y = tmp_df.population
     
@@ -36,8 +56,8 @@ for c in selected_countries:
     plt.plot(x, y)
     plt.title("Global population of " + c)
 
+    # Add the plot into the new dictionary, coupled to its country name
     nested_population_plots_dict[c] = fig_handle
 
-
+# Picle the entire plots dictionary and write it to disk
 pickle.dump( nested_population_plots_dict, open("nested_population_plots_dict.pkl", "wb") )
-
