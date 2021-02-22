eda852875d9a9b10d54c4f3085fa98f19309e4cc
Luca Zavarella <lucazavarella@outlook.com>
2021-02-22 09:21:51 +0100

    implemented also the loading of PKL files in Python Visuals

---
+++ Chapter04/importing-pkl-files/07-deserialize-plots-df-into-python-visual new.py
@@ -0,0 +1,48 @@
+
+# Python Visual truncates the dataframe values to 32766 characters max
+
+import pandas as pd
+import pickle
+import matplotlib.pyplot as plt
+
+
+def serialize(obj):
+    return obj.encode('latin1')
+
+def unserialize(obj):
+    return obj.decode('latin1')
+
+
+def toUnpickle(obj):
+    return pickle.loads(obj)
+
+
+def toUncut(obj):
+    return "".join(obj)
+
+
+# # For debug purposes
+# # In Power BI a single plot is selected using a slicer
+# dataset = plots_df.loc[ plots_df['country_name'] == "France" ]
+
+
+dataset = dataset.sort_values(['chunk_id'], ascending=[True])
+dataset = dataset.reset_index(drop=True)
+
+dataset_fixed = dataset
+dataset_fixed.loc[:,'plot_str'] = dataset['plot_str'].apply( lambda x: x + ' ' if len(x) == 31999 else x)
+
+str_merged = toUncut(dataset_fixed['plot_str'])
+
+str_int_list = list(map(int, str_merged.split()))
+
+str_pickled_byte_string = bytearray(str_int_list)
+
+str_unpickled = toUnpickle(str_pickled_byte_string)
+
+mng = plt.get_current_fig_manager()
+mng.figure = str_unpickled
+
+plt.figure = str_unpickled
+
+str_unpickled.show()
---
+++ Chapter04/importing-pkl-files/debug-pkl-deserialization.py
@@ -0,0 +1,68 @@
+# Python Visual truncates the dataframe values to 32766 characters max
+
+import pandas as pd
+import pickle
+
+project_folder = "D:\\LZavarella\\OneDrive\\MVP\\Packt Book\\Code\\Extending-Power-BI-with-Python-and-R\\Chapter04\\importing-pkl-files"
+
+dataset_debug = pd.read_csv( open(os.path.join(project_folder, "input_df_python_visual.csv"), "rb") )
+
+def serialize(obj):
+    return obj.encode('latin1')
+
+def unserialize(obj):
+    return obj.decode('latin1')
+
+
+def toUnpickle(obj):
+    return pickle.loads(obj)
+
+
+def toUncut(obj):
+    return "".join(obj)
+
+
+
+dataset_debug = dataset_debug.sort_values(['chunk_id'], ascending=[True])
+dataset_debug = dataset_debug.reset_index(drop=True)
+
+for i in range(0, len(dataset['plot_str'])):
+    dataset_debug['plot_str'][i] == dataset['plot_str'][i]
+
+for i in range(0, len(dataset['plot_str'])):
+    if len(dataset_debug['plot_str'][i]) != len(dataset['plot_str'][i]):
+        arrow = '  <--'
+    else:
+        arrow = ''
+
+    print( 'Len debug (%s) = %s  |  Len orig (%s) = %s %s' % (i, len(dataset_debug['plot_str'][i]), i, len(dataset['plot_str'][i]), arrow) )
+
+# # Get strings to compare in Notepad++
+# dataset['plot_str'][7]
+# dataset_debug['plot_str'][7]
+
+# dataset['plot_str'][10]
+# dataset_debug['plot_str'][10]
+
+dataset_debug_fixed = dataset_debug
+dataset_debug_fixed.loc[:,'plot_str'] = dataset_debug['plot_str'].apply( lambda x: x + ' ' if len(x) == 31999 else x)
+
+for i in range(0, len(dataset['plot_str'])):
+    if len(dataset_debug_fixed['plot_str'][i]) != len(dataset['plot_str'][i]):
+        arrow = '  <--'
+    else:
+        arrow = ''
+
+    print( 'Len debug (%s) = %s  |  Len orig (%s) = %s %s' % (i, len(dataset_debug_fixed['plot_str'][i]), i, len(dataset['plot_str'][i]), arrow) )
+
+
+
+str_merged_debug_fixed = toUncut(dataset_debug_fixed['plot_str'])
+
+str_int_list_debug_fixed = list(map(int, str_merged_debug_fixed.split()))
+
+str_pickled_byte_string_debug_fixed = bytearray(str_int_list_debug_fixed)
+
+str_unpickled_debug_fixed = toUnpickle(str_pickled_byte_string_debug_fixed)
+
+str_unpickled_debug_fixed.show()
