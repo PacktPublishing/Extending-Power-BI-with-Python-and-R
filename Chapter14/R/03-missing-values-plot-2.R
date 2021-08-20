9bfe1eeedb3ba3b85039600b31d3756fbe3ef934
Luca Zavarella <lucazavarella@outlook.com>
2021-08-20 16:15:31 +0200

    changed the UpSet plot text size

--- Chapter14/R/03-missing-values-plot-2.R
+++ Chapter14/R/03-missing-values-plot-2.R
@@ -3,8 +3,8 @@ library(ggplot2)
 library(naniar)
 
 # Load the dataset with proper column data types
-init_path <- r'{C:\<your-folder>\Chapter14\R\00-init-dataset.R}'
+init_path <- r'{C:\Users\LucaZavarella\OneDrive\MVP\PacktBook\Code\Extending-Power-BI-with-Python-and-R\Chapter14\R\00-init-dataset.R}'
 if(!exists('foo', mode='function')) source(init_path)
 
 
-gg_miss_upset(tbl)
+gg_miss_upset(tbl, text.scale = 2)
